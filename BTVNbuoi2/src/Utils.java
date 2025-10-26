15be5d1dee02732352886ec99a0f80780738de60
TienDung2105 <Ntdnqb2006@gmail.com>
2025-10-26 19:21:00 +0700

    baitapvenhabuoi2

--- BTVNbuoi2/src/Utils.java
+++ BTVNbuoi2/src/Utils.java
@@ -1,52 +1,51 @@
 public class Utils {
 
     public static int tinhtong(int a[], int n){
-        int tong =0;
-        for(int i=0;i<n;i++){
-            tong+=a[i];
+        int tong = 0;
+        for(int i=0; i<n; i++){
+            tong += a[i];
         }
-        return tong ;
+        return tong;
     }
 
     public static int timMin(int a[], int n){
-        int min =a[0];
-        for(int i=1;i<n;i++){
-            if(a[i]<min){
-                min=a[i];
+        int min = a[0];
+        for(int i=1; i<n; i++){
+            if(a[i] < min){
+                min = a[i];
             }
         }
         return min;
     }
 
     public static int timMax(int a[], int n){
-        int max=a[0];
-        for(int i=1;i<n;i++){
-            if(max<a[i]){
-                max=a[i];
+        int max = a[0];
+        for(int i=1; i<n; i++){
+            if(a[i] > max){
+                max = a[i];
             }
         }
         return max;
     }
 
-    public static int sapxep (int a[], int n){
-        for(int i=0;i<n-1;i++){
-            for(int j=0;j<n-1-i;j++){
-                if(a[j]>a[j+1]){
-                    int temp= a[j];
-                    a[j]=a[j+1];
-                    a[j+1]=temp;
+    public static void sapxep(int a[], int n){
+        for(int i=0; i<n-1; i++){
+            for(int j=0; j<n-1-i; j++){
+                if(a[j] > a[j+1]){
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
                 }
             }
         }
-        return 0;
     }
 
     public static boolean laSoNT(int n){
-        if(n<2){
+        if(n < 2) {
             return false;
         }
-        for(int i=2;i<Math.sqrt(n);i++){
-            if(n%i==0){
+        for(int i=2; i<=Math.sqrt(n); i++){
+            if(n % i == 0){
                 return false;
             }
         }
@@ -54,9 +53,9 @@ public class Utils {
     }
 
     public static int inSoNTMax(int a[], int n){
-        int maxNT=-1;
-        for(int i=0;i<n;i++) {
-            if (laSoNT(a[i]) && maxNT<a[i]) {
+        int maxNT = -1;
+        for(int i=0; i<n; i++) {
+            if(laSoNT(a[i]) && a[i] > maxNT) {
                 maxNT = a[i];
             }
         }
@@ -64,11 +63,9 @@ public class Utils {
     }
 
     public static void inmang(int a[], int n){
-        for(int i=0;i<n;i++){
-            System.out.println(a[i]+" ");
+        for(int i=0; i<n; i++){
+            System.out.print(a[i] + " ");
         }
         System.out.println();
     }
-
 }
-
